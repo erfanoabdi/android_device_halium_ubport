d2244117075f7de984c215fe09b444a6b688ef69
Erfan Abdi <erfangplus@gmail.com>
2020-03-29 20:18:41 +0430

    ubport: Init tree

---
+++ README.md
@@ -0,0 +1,3 @@
+
+Device configuration for Halium ubuntu touch (ubport)
+==================================
---
+++ rootdir/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+# Init scripts
+
+include $(CLEAR_VARS)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.halium.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.halium.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
