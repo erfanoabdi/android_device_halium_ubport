2137515098b4cb3353c9e0e306d748836073be03
Erfan Abdi <erfangplus@gmail.com>
2020-05-14 15:20:39 +0430

    Import Keymaster4 from TWRP

---
+++ Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fake_crypt
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_CFLAGS += \
+    -Wno-unused-variable \
+    -Wno-sign-compare \
+    -Wno-unused-parameter \
+    -Wno-comment
+
+LOCAL_SRC_FILES += Keymaster4.cpp
+LOCAL_SHARED_LIBRARIES += \
+    android.hardware.keymaster@4.0 \
+    libkeymaster4support
+
+include $(BUILD_EXECUTABLE)
